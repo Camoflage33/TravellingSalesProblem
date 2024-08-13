23f5ab1d2f06ea026aa9547f12078d65340c942a
Administrator <Administrator@DESKTOP-M4QG6HO>
2024-08-12 19:03:51 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TSPJamaica
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, (double latitude, double longitude)> parishCoordinates;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeParishes();
+            InitializeCoordinates();
+        }
+
+        private void InitializeParishes()
+        {
+            string[] parishes = { "Kingston", "St. Andrew", "St. Catherine", "Clarendon", "Manchester", "St. Elizabeth", "Westmoreland", "Hanover", "St. James", "Trelawny", "St. Ann", "St. Mary", "Portland", "St. Thomas" };
+            StartVertexComboBox.Items.AddRange(parishes);
+            EndVertexComboBox.Items.AddRange(parishes);
+        }
+
+        private void InitializeCoordinates()
+        {
+            parishCoordinates = new Dictionary<string, (double latitude, double longitude)>
+            {
+                { "Kingston", (17.9712, -76.7936) },
+                { "St. Andrew", (18.0016, -76.7442) },
+                { "St. Catherine", (17.9648, -76.8768) },
+                { "Clarendon", (18.0308, -77.2167) },
+                { "Manchester", (18.0426, -77.5071) },
+                { "St. Elizabeth", (18.0055, -77.8541) },
+                { "Westmoreland", (18.1923, -78.1334) },
+                { "Hanover", (18.4057, -78.1334) },
+                { "St. James", (18.4762, -77.8939) },
+                { "Trelawny", (18.3268, -77.6595) },
+                { "St. Ann", (18.4298, -77.1971) },
+                { "St. Mary", (18.2312, -76.8782) },
+                { "Portland", (18.1783, -76.4079) },
+                { "St. Thomas", (17.9147, -76.4093) }
+            };
+        }
+
+        private double CalculateHeuristic(string currentParish, string goalParish)
+        {
+            var current = parishCoordinates[currentParish];
+            var goal = parishCoordinates[goalParish];
+
+            // Calculation of the distance
+            double distance = Math.Sqrt(Math.Pow(current.latitude - goal.latitude, 2) + Math.Pow(current.longitude - goal.longitude, 2));
+
+            return distance;
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (StartVertexComboBox.SelectedIndex == -1 || EndVertexComboBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select both start and end vertices.");
+                return;
+            }
+
+            string startParish = StartVertexComboBox.SelectedItem.ToString();
+            string endParish = EndVertexComboBox.SelectedItem.ToString();
+
+            int startVertex = StartVertexComboBox.SelectedIndex;
+            int endVertex = EndVertexComboBox.SelectedIndex;
+
+            
+            int[,] graph = new int[,]
+            {
+                { 0, 8, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20 },
+                { 8, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5 },
+                { 25, 25, 0, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 32, 0, 38, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 38, 0, 27, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 27, 0, 30, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 30, 0, 26, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 26, 0, 18, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 18, 0, 22, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 22, 0, 20, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 19, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 19, 0, 25, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 22 },
+                { 20, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 22, 0 }
+            };
+
+            Graph g = new Graph();
+            string path = "";
+            int distance = 0;
+            Stopwatch stopwatch = new Stopwatch();
+
+            if (GreedySearchRadioButton.Checked)
+            {
+                stopwatch.Start();
+                distance = g.Dijkstra(graph, startVertex, endVertex, out path);
+                stopwatch.Stop();
+                DisplayLabel.Text = $"Dijkstra's Algorithm selected\nFrom: {StartVertexComboBox.SelectedItem}\nTo: {EndVertexComboBox.SelectedItem}\nDistance: {distance}\nPath: {path}\nTime: {stopwatch.ElapsedMilliseconds} ms";
+            }
+            else if (AStarRadioButton.Checked)
+            {
+                stopwatch.Start();
+                distance = g.AStar(graph, startVertex, endVertex, out path);
+                stopwatch.Stop();
+                DisplayLabel.Text = $"A* Search Algorithm selected\nFrom: {StartVertexComboBox.SelectedItem}\nTo: {EndVertexComboBox.SelectedItem}\nDistance: {distance}\nPath: {path}\nTime: {stopwatch.ElapsedMilliseconds} ms";
+            }
+            else if (BestFirstSearchRadioButton.Checked)
+            {
+                stopwatch.Start();
+                distance = g.BestFirstSearch(graph, startVertex, endVertex, out path);
+                stopwatch.Stop();
+                DisplayLabel.Text = $"Best First Search Algorithm selected\nFrom: {StartVertexComboBox.SelectedItem}\nTo: {EndVertexComboBox.SelectedItem}\nDistance: {distance}\nPath: {path}\nTime: {stopwatch.ElapsedMilliseconds} ms";
+            }
+            else
+            {
+                MessageBox.Show("Please select a search method.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void pictureBox1_Click(object sender, EventArgs e) { }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+    }
+}
---
+++ Graph.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TSPJamaica
+{
+    public class Graph
+    {
+        private const int V = 14; // Number of vertices in the graph
+        private Dictionary<int, (double latitude, double longitude)> coordinates;
+
+        public Graph()
+        {
+            InitializeCoordinates();
+        }
+
+        void InitializeCoordinates()
+        {
+            coordinates = new Dictionary<int, (double, double)>
+            {
+                { 0, (18.0127, -76.7959) }, // Kingston
+                { 1, (18.4665, -77.9211) }, // Montego Bay
+                { 2, (17.9896, -76.9442) }, // Portmore
+                { 3, (18.0153, -77.4996) }, // Mandeville
+                { 4, (18.4172, -78.0748) }, // Negril
+                { 5, (18.4047, -77.8558) }, // Lucea
+                { 6, (18.2748, -78.3447) }, // Savanna-la-Mar
+                { 7, (18.1004, -77.2855) }, // Black River
+                { 8, (18.1772, -77.0151) }, // May Pen
+                { 9, (18.2701, -77.6260) }, // Falmouth
+                { 10, (18.4417, -77.6592) }, // Runaway Bay
+                { 11, (18.4514, -77.1210) }, // Ocho Rios
+                { 12, (18.4092, -76.9696) }, // St. Ann's Bay
+                { 13, (18.1760, -76.7922) }  // Port Antonio
+            };
+        }
+
+        double CalculateHeuristic(int u, int v)
+        {
+            var coordU = coordinates[u];
+            var coordV = coordinates[v];
+
+            double heuristic = Math.Sqrt(Math.Pow(coordU.latitude - coordV.latitude, 2) + Math.Pow(coordU.longitude - coordV.longitude, 2));
+            return heuristic;
+        }
+
+        public int Dijkstra(int[,] graph, int startVertex, int endVertex, out string path)
+        {
+            int[] dist = new int[V];
+            bool[] shortestPathTreeSet = new bool[V];
+            int[] parents = new int[V];
+
+            for (int i = 0; i < V; i++)
+            {
+                dist[i] = int.MaxValue;
+                shortestPathTreeSet[i] = false;
+                parents[i] = -1;
+            }
+
+            dist[startVertex] = 0;
+
+            for (int count = 0; count < V - 1; count++)
+            {
+                int u = MinDistance(dist, shortestPathTreeSet);
+                shortestPathTreeSet[u] = true;
+
+                for (int v = 0; v < V; v++)
+                {
+                    if (!shortestPathTreeSet[v] && graph[u, v] != 0 && dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+                    {
+                        dist[v] = dist[u] + graph[u, v];
+                        parents[v] = u;
+                    }
+                }
+            }
+
+            path = GetPath(parents, endVertex);
+            return dist[endVertex];
+        }
+
+        int MinDistance(int[] dist, bool[] sptSet)
+        {
+            int min = int.MaxValue, minIndex = -1;
+
+            for (int v = 0; v < V; v++)
+            {
+                if (!sptSet[v] && dist[v] <= min)
+                {
+                    min = dist[v];
+                    minIndex = v;
+                }
+            }
+
+            return minIndex;
+        }
+
+        string GetPath(int[] parents, int j)
+        {
+            List<int> pathList = new List<int>();
+            int current = j;
+
+            while (current != -1)
+            {
+                pathList.Add(current);
+                current = parents[current];
+            }
+
+            pathList.Reverse(); // To get the path from start to end
+            return string.Join(" -> ", pathList);
+        }
+
+
+        public int AStar(int[,] graph, int startVertex, int endVertex, out string path)
+        {
+            int[] dist = new int[V];
+            bool[] closedList = new bool[V];
+            int[] parents = new int[V];
+
+            for (int i = 0; i < V; i++)
+            {
+                dist[i] = int.MaxValue;
+                closedList[i] = false;
+                parents[i] = -1;
+            }
+
+            dist[startVertex] = 0;
+
+            SortedSet<(double, int)> openList = new SortedSet<(double, int)>();
+            openList.Add((CalculateHeuristic(startVertex, endVertex), startVertex));
+
+            while (openList.Count > 0)
+            {
+                var (f, u) = openList.Min;
+                openList.Remove(openList.Min);
+                closedList[u] = true;
+
+                if (u == endVertex)
+                {
+                    path = GetPath(parents, endVertex);
+                    return dist[u];
+                }
+
+                for (int v = 0; v < V; v++)
+                {
+                    if (!closedList[v] && graph[u, v] != 0)
+                    {
+                        double g = dist[u] + graph[u, v];
+                        double h = CalculateHeuristic(v, endVertex);
+                        double fNew = g + h;
+
+                        if (g < dist[v])
+                        {
+                            dist[v] = (int)g;
+                            parents[v] = u;
+                            openList.Add((fNew, v));
+                        }
+                    }
+                }
+            }
+
+            path = "";
+            return -1; // If no path found
+        }
+
+        public int BestFirstSearch(int[,] graph, int startVertex, int endVertex, out string path)
+        {
+            bool[] visited = new bool[V];
+            int[] parents = new int[V];
+            for (int i = 0; i < V; i++)
+            {
+                parents[i] = -1;
+            }
+
+            SortedSet<(double, int)> openList = new SortedSet<(double, int)>();
+            openList.Add((CalculateHeuristic(startVertex, endVertex), startVertex));
+
+            while (openList.Count > 0)
+            {
+                var (h, u) = openList.Min;
+                openList.Remove(openList.Min);
+
+                if (u == endVertex)
+                {
+                    path = GetPath(parents, endVertex);
+                    return (int)h;
+                }
+
+                visited[u] = true;
+
+                for (int v = 0; v < V; v++)
+                {
+                    if (!visited[v] && graph[u, v] != 0)
+                    {
+                        double heuristic = CalculateHeuristic(v, endVertex);
+                        if (parents[v] == -1) // Not yet visited
+                        {
+                            openList.Add((heuristic, v));
+                            parents[v] = u;
+                        }
+                    }
+                }
+            }
+
+            path = "";
+            return -1; // If no path found
+        }
+
+    }
+}
